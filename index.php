65bc5e95f61f9844f52fe6a3e632c4171c98029c
JOYRU <joycseru@gmail.com>
2019-10-24 09:54:59 -0600

    designing and developing a website for a local small Shopping Mall using html,css,javascript and raw php.

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <link rel="stylesheet" type="text/css" href="css/index.css" media="screen,projection" />
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
+  
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Rimjhim Shopping Mall</title>
+</head>
+
+<body>
+  <div id="container">
+    <div id="header">
+      <h1>Rimjhim Shopping Mall</h1>
+       Help to You to be smart
+      
+      
+    </div>
+    <div id="subheader">
+      <h2>Home</h2>
+      <ul id="navigation">
+        <li class="active">Home</li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </div>
+    
+    <div id="content">
+      <h3>Overview</h3>
+      The business is simply a small shopping Mall; located in Allupotti, Rajshahi, Bangladesh.
+      <br/><br/>
+      <h3>Most Recent Announcement</h3>
+      <?php // echo getRecentAnnouncement(); ?>
+      <br /><br />
+      Click on the link, to see all <a href="#">Announcements</a> ordered newest to oldest.
+      <br/><br/>
+      <h3>Login Form</h3>
+      </div>
+    
+    <div id="subcontent">
+      An overview of the business is given on this page along with the most recent announcement made.
+      <br/><br/>
+      There is also a login form which will be used by both the user and the admin.
+    </div>
+    <div id="footer">
+      Small Food Restaurant Business Website. (Group Project).
+    </div>
+    </div>
+</body>
+</html>
